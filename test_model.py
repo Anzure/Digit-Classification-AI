187053abcc6656b8fea35790abba5a524a9b66e8
André Mathisen <9752808+anzure@users.noreply.github.com>
2021-06-19 17:46:15 +0200

    🎨 Optimized code and UI.

---
+++ test_model.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_image(target_path):
+    color_image = cv2.imread(target_path, cv2.IMREAD_ANYCOLOR)
+    color_image = cv2.resize(color_image, (28, 28))
+    grey_image = cv2.imread(target_path, cv2.IMREAD_GRAYSCALE)
+    grey_image = cv2.resize(grey_image, (28, 28))
+    grey_image = grey_image.astype('float32')
+    image = grey_image.reshape(1, 28, 28, 1)
+    image = 255 - image
+    image /= 255
+    return image, grey_image, color_image
+
+
+file_path = r'test1.png'
+test_data = get_image(file_path)
+test_image = test_data[0]
+
+model = tf.keras.models.load_model('tallgjennkjenner.model')
+predictions = model.predict([test_image])
+test_result = np.argmax(predictions[0])
+print("Resultat", test_result)
+
+plt.subplot(2, 1, 1)
+plt.xticks([])
+plt.yticks([])
+plt.imshow(test_data[2], cmap=plt.cm.binary)
+plt.xlabel("Test bilde")
+plt.subplot(2, 1, 2)
+plt.xticks([])
+plt.yticks([])
+plt.imshow(test_data[1], cmap=plt.cm.binary)
+plt.xlabel(f"Resultat: {test_result}")
+plt.show()
---
+++ train_model.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# Prepare data
+dataset_mnist = keras.datasets.mnist
+(training_images, training_labels), (testing_images, testing_labels) = dataset_mnist.load_data()
+training_images = keras.utils.normalize(training_images, axis=1)
+testing_images = keras.utils.normalize(testing_images, axis=1)
+
+# Neural network
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten())  # 28x28 -> 1x784
+model.add(keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(keras.layers.Dense(10, activation=tf.nn.softmax))
+
+# Train model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=3)
+
+# Print results
+val_loss, val_acc = model.evaluate(testing_images, testing_labels)
+print("Feilmargin", val_loss * 100, "%")
+print("Nøyaktighet", val_acc * 100, "%")
+
+# Save model
+model.save('tallgjennkjenner.model')
